ea5f0c217e31b8a925a9e77cc2f6ce3fe7ad493c
JOAO-07 <97554993+JOAO-07@users.noreply.github.com>
2022-02-21 17:29:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const hex = document.querySelector(".hex")
+const btn = document.querySelector(".generate")
+    
+const generatecolor = () => {
+  const randomColor = Math.random().toString(16).substring
+  (2, 8);
+  document.body.style.backgroundColor = "#" + randomColor;
+  hex.innerHTML = "#" + randomColor;
+};
+
+btn.addEventListener("click", generatecolor);
+
+generatecolor();
+
+//let color = Math.random();
+//color  = Math.random().toString(16);
+//color  = Math.random().toString(16).substring(2, 8 ); 
+
+//console.log(color);
